768dfc800cd490d6908698840fb82d710380d90c
bc l <774531697@qq.com>
2019-05-16 18:07:25 +0800

    1

--- Graph/DenseGraph.h
+++ Graph/DenseGraph.h
@@ -1,1 +1,40 @@
-#pragma once
+#include<vector>
+#include<cassert>
+
+
+
+
+using namespace std;
+
+//³íÃÜÍ¼    --ÁÚ½Ó¾ØÕó
+//dense graph -- Adjacency matrjix
+class DenseGraph {
+
+private:
+	int n, m;
+	bool directed;
+	vector < vector<bool>> g;
+
+public:
+	DenseGraph(int n, bool directed) {
+
+		this->n = n;
+		this->m = 0;
+		this->directed = directed;
+		for (int i = 0; i < n; i++)
+			g.push_back(vector<bool>(n, false));
+	}
+
+	~DenseGraph()
+	{
+
+	}
+
+	int V() { return n; }
+	int E() { return m; }
+
+	void addEdge(int v, int w) {
+
+
+	}
+};
